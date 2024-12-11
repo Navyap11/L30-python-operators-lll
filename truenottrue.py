4e1c4eeb3de60f6e13503d74f6f40796a98b390c
navyap11 <ashwin.p.parmar@gmail.com>
2024-12-11 15:35:46 +0100

    hill

---
+++ truenottrue.py
@@ -0,0 +1,24 @@
+x=5
+if type (x) is int:
+    print("integer")
+else:
+    print("decimal")
+
+a= 6.7
+
+if type(a) is float:
+    print("Decimal")
+else:
+    print("Interger")
+
+n= 45
+y=45
+if (n is y):
+    print("Same")
+else:
+    print("Different")
+n=20
+if (n is y):
+    print("Same")
+else:
+    print("Different")
